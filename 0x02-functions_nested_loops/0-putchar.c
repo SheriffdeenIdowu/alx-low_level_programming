701c124707f53ec1a04b780715d5ef03836471f9
SheriffdeenIdowu <111003537+SheriffdeenIdowu@users.noreply.github.com>
2022-09-13 17:31:03 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-  * main - prints the string "main"
-  *
-  * Return: 0 on success.
-  */
-
+ * main -  prints -Putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char *str;
-	int i;
-
-	str = "Holberton";
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
