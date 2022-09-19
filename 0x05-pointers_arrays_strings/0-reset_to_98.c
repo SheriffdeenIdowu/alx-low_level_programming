1d81d7d43ced9fe91a9b0ed3d3201e0d3fba7d24
SheriffdeenIdowu <111003537+SheriffdeenIdowu@users.noreply.github.com>
2022-09-19 18:20:47 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,15 @@
-#include <main.h>
+#include "main.h"
+
 /**
- *
+ *reset_to_98- it reset to 98
+ *@n: checked
+ *Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	int *ptr;
+
+	ptr = n;
+
+	*ptr = 98;
+}
