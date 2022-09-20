c76d15d70294473011f3774dcb93d0001f823f32
SheriffdeenIdowu <111003537+SheriffdeenIdowu@users.noreply.github.com>
2022-09-20 10:20:13 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,37 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-#include "main.h"
-
+#include <stdio.h>
 /**
- * main - the program generate a 9 digits of random number as password
- * @n: a random number
- * @sum: a array of floor string that cover all alphabet character.
- * @i: a char array index.
- *
- * Return: return 0.
  */
-
 int main(void)
 {
-	int i, n;
-	unsigned long int sum;
-	char password[100];
+int num;
 
-	n = i = sum = 0;
-	char al[63] = "0123456789ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	srand(time(NULL));
-	while (sum < (2772 - 122))
-	{
-		n = rand() % 62;
-		password[i] = al[n];
-		sum = sum + password[i];
-		i++;
-	}
-	n = 2772 - sum;
-	password[i] = n;
-	password[++i] = '\0';
-	printf("%s\n", password);
-	return (0);
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 }
